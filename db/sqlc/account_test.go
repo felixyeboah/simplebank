f1758d8147cbf8a20be67746f1bacba6381fc339
Felix Nana Yaw Yeboah - Jefferson <87368189+felixyeboah@users.noreply.github.com>
2024-06-13 10:46:31 +0000

    added tests for creating an account

---
+++ db/sqlc/account_test.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	"simplebank/util"
+	"testing"
+)
+
+func TestCreateAccount(t *testing.T) {
+	args := CreateAccountParams{
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
+	}
+
+	account, err := testQueries.CreateAccount(context.Background(), args)
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+
+	require.Equal(t, args.Owner, account.Owner)
+	require.Equal(t, args.Balance, account.Balance)
+	require.Equal(t, args.Currency, account.Currency)
+
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
+}
---
+++ util/random.go
@@ -0,0 +1,44 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alpabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func init() {
+	// Seed the random number generator
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandomInt(min, max int) int {
+	return min + rand.Intn(max-min+1)
+}
+
+func RandomString(n int) string {
+	var stringBuilder strings.Builder
+	k := len(alpabets)
+
+	for i := 0; i < n; i++ {
+		c := alpabets[rand.Intn(k)]
+		stringBuilder.WriteByte(c)
+	}
+
+	return stringBuilder.String()
+}
+
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomMoney() int64 {
+	return int64(RandomInt(0, 1000))
+}
+
+func RandomCurrency() string {
+	currencies := []string{"USD", "EUR", "CAD"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
